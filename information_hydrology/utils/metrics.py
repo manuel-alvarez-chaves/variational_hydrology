be4137359973971ea6b071db948d79a3a6e71f98
manuel-alvarez-chaves <malvarez062@gmail.com>
2024-11-13 15:38:37 -0700

    LossNLL for mixtures

--- information_hydrology/utils/metrics.py
+++ information_hydrology/utils/metrics.py
@@ -14,16 +14,18 @@ def loss_mse(y_hat: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
 def loss_kld(mu: torch.Tensor, logvar: torch.Tensor) -> torch.Tensor:
     return -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
-def loss_nll(y_hat: torch.Tensor | Tuple[torch.Tensor], y: torch.Tensor, samples=True) -> torch.Tensor:
-    if samples:
+def loss_nll(y_hat: torch.Tensor | Tuple[torch.Tensor], y: torch.Tensor) -> torch.Tensor:
+    if type(y_hat) is tuple:
+        mu, sigma, w = y_hat
+    else:
         mu = torch.mean(y_hat, dim=1)
         sigma = torch.std(y_hat, dim=1)
-    else:
-        mu, sigma = y_hat
+        w = torch.ones_like(mu)
 
     p = - 0.5 * ((y - mu) / sigma) ** 2
     p = torch.exp(p) / (sigma * np.sqrt(2 * np.pi))
-    loss = - torch.log(torch.sum(p, dim=-1) + 1e-10)
+    p = (p * w).sum(dim=-1)
+    
+    loss = - torch.log(p + 1e-10)
     loss = torch.mean(loss)
-    # loss = nn.GaussianNLLLoss()(mu, y, sigma)
     return loss
