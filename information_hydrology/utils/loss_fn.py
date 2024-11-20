bcca05083ad0b66868868db1dff3f84428955f9a
manuel-alvarez-chaves <malvarez062@gmail.com>
2024-11-20 15:38:38 -0700

    Fixes for NaNs

--- information_hydrology/utils/loss_fn.py
+++ information_hydrology/utils/loss_fn.py
@@ -6,7 +6,7 @@ from torch import nn
 
 
 def loss_mse(y_hat: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
-    mask = ~torch.isnan(y) & ~torch.isnan(y_hat)
+    mask = ~(np.isnan(y) | np.isnan(y_hat))
     y_hat = y_hat[mask]
     y = y[mask]
     return nn.functional.mse_loss(y_hat, y)
@@ -17,11 +17,15 @@ def loss_kld(mu: torch.Tensor, logvar: torch.Tensor) -> torch.Tensor:
 def loss_nll(y_hat: torch.Tensor | Tuple[torch.Tensor], y: torch.Tensor) -> torch.Tensor:
     if type(y_hat) is tuple:
         mu, sigma, w = y_hat
+        mask = ~(y.isnan() | mu.sum(-1).reshape(-1, 1).isnan()).flatten()
+        mu, sigma, w = mu[mask], sigma[mask], w[mask]
     else:
-        mu = torch.mean(y_hat, dim=1)
-        sigma = torch.std(y_hat, dim=1)
+        mask = ~(y.isnan() | y_hat.sum(dim=1).isnan()).flatten()
+        y_hat = y_hat[mask, ...]
+        mu, sigma = y_hat.mean(dim=1), y_hat.std(dim=1)
         w = torch.ones_like(mu)
 
+    y = y[mask, :]
     p = - 0.5 * ((y - mu) / sigma) ** 2
     p = torch.exp(p) / (sigma * np.sqrt(2 * np.pi))
     p = (p * w).sum(dim=-1)
