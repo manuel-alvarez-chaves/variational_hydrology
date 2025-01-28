a3b61091d1b72ea7bb674236ae4df6d149922e8d
manuel-alvarez-chaves <malvarez062@gmail.com>
2025-01-28 11:10:33 +0100

    Add streamflow (just in case)

--- dashboard/generate_data_file.py
+++ dashboard/generate_data_file.py
@@ -8,15 +8,22 @@ import pyeto
 
 # Read CAMELS-US file
 path_daymet = Path("../CAMELS-US/basin_mean_forcing/daymet")
-path_data = path_daymet / "14" / "09081600_lump_cida_forcing_leap.txt"
+path_streamflow = Path("../CAMELS-US/usgs_streamflow")
+
+path_forcing_data = path_daymet / "14/09081600_lump_cida_forcing_leap.txt"
+path_qobs = path_streamflow / "14/09081600_streamflow_qc.txt"
 
 # Choose period of analysis
 selected_period = pd.date_range(start="1998-04-01", end="1999-10-01", freq="D")
 
-# Process file
-with path_data.open("r") as f:
-    lat_rad = float(f.readline()) * (np.pi / 180)  # First line is latitude [in degrees]
-data = pd.read_csv(path_data, skiprows=3, delimiter="\s+")
+# Process forcing
+with path_forcing_data.open("r") as f:
+    lat_rad = float(f.readline()) * (
+        np.pi / 180
+    )  # First line is latitude [in degrees]
+    f.readline()  # Skip second line
+    area = float(f.readline())  # Second line is area [in square meters]
+data = pd.read_csv(path_forcing_data, skiprows=3, delimiter="\s+")
 
 # Set datetime as index
 datetimes = pd.to_datetime(
@@ -59,8 +66,25 @@ for idx, d in enumerate(doy):
 
 data["eto(mm/day)"] = eto
 
+# Process streamflow
+col_names = ["basin", "Year", "Mnth", "Day", "QObs", "flag"]
+data_qobs = pd.read_csv(path_qobs, sep=r"\s+", header=None, names=col_names)
+datetimes = pd.to_datetime(
+    {
+        "year": data_qobs["Year"],
+        "month": data_qobs["Mnth"],
+        "day": data_qobs["Day"],
+    }
+)
+data_qobs = data_qobs.set_index(datetimes)
+data_qobs = data_qobs.loc[selected_period]
+qobs = 28316846.592 * data_qobs["QObs"].values * 86400 / (area * 10**6)
+
+# Extract selected period
+data["qobs(mm/day)"] = qobs
+
 # Clean up
-columns_to_keep = ["prcp(mm/day)", "eto(mm/day)"]
+columns_to_keep = ["prcp(mm/day)", "eto(mm/day)", "qobs(mm/day)"]
 data = data[columns_to_keep]
 
 # Save to CSV
