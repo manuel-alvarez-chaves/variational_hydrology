02a211464f2c10dea95ffae7d5cfa9447d608b76
manuel-alvarez-chaves <malvarez062@gmail.com>
2025-03-27 18:25:53 +0100

    AUC over the whole curve

--- src/information_hydrology/utils/metrics.py
+++ src/information_hydrology/utils/metrics.py
@@ -20,9 +20,7 @@ def calc_cdf(metric: list):
     x = np.sort(metric)
     y = np.arange(1, len(metric) + 1) / len(metric)
     median = np.median(x)
-    idx_nonzero = sum(x < 0)
-    auc = np.trapezoid(y[idx_nonzero:], x[idx_nonzero:])
-    # auc = np.trapezoid(y, x)
+    auc = np.trapezoid(y, x)
 
     return {"x": x, "y": y, "median": median, "auc": auc}
 
