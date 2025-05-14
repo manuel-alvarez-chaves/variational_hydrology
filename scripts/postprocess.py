be83de91eaf949e6fe19bf3389e344dd4d39678f
manuel-alvarez-chaves <malvarez062@gmail.com>
2025-05-14 19:15:47 +0200

    New 'postprocess' script

--- scripts/postprocess.py
+++ scripts/postprocess.py
@@ -14,6 +14,7 @@ from hy2dl.modelzoo.cudalstm import CudaLSTM
 from information_hydrology.modelzoo.mdn import LSTMMDN
 from information_hydrology.modelzoo.vlstm import VLSTM, ErrorMode, SamplingMode
 from information_hydrology.utils.distributions import Distribution
+from information_hydrology.utils.loss_fn import loss_nll
 from information_hydrology.utils.metrics import calc_crps, calc_kde_loglik
 from neuralhydrology.evaluation import metrics as calc_metrics
 from torch.utils.data import DataLoader
@@ -99,9 +100,9 @@ def generate_netcdf(path_to_model_dict: Path) -> Tuple[str, xr.Dataset]:
         ds.calculate_basin_std()
         ds.standardize_data(standardize_output=False)
 
-        loader = DataLoader(ds, batch_size=256, shuffle=False, collate_fn=ds.collate_fn)
+        loader = DataLoader(ds, batch_size=1024, shuffle=False, collate_fn=ds.collate_fn)
         
-        dates, y_obs, y_hat = [], [], []
+        dates, y_obs, y_hat, loc, scale, kappa, w = [], [], [], [], [], [], []
         for sample in loader:
             # Fix inputs
             x_d, x_s, y, _, _, date = sample.values()
@@ -118,14 +119,31 @@ def generate_netcdf(path_to_model_dict: Path) -> Tuple[str, xr.Dataset]:
                 case "vLSTM":
                     pred = model.sample(x, num_samples, mode=SamplingMode.LEARNED, track_grad=False) # [batch_size, num_samples, num_targets]
                     y_hat_sample = pred.detach().cpu().clone().numpy()[:, :, 0] # [batch_size, num_samples]
-                case "LSTMGMM" | "LSTMCMAL":
-                    pred = model.sample(x, num_samples)
-                    pred[:, 0] = model.calc_mean(x)
-                    y_hat_sample = pred # [batch_size, num_samples]
-            
+                case "LSTMCMAL":
+                    pred = model(x)
+                    params, w_batch = pred
+                    loc_batch, scale_batch, kappa_batch = params
+                    y_hat_sample = model.sample(x, num_samples) # [batch_size, num_samples]
+                    y_hat_sample[:, 0] = model.mean(x) # [batch_size] 
+                case "LSTMGMM":
+                    pred = model(x)
+                    params, w_batch = pred
+                    loc_batch, scale_batch, _ = params
+                    y_hat_sample = model.sample(x, num_samples) # [batch_size, num_samples]
+                    y_hat_sample[:, 0] = model.mean(x) # [batch_size] 
+
             dates.append(date) # list of num_batches
             y_obs.append(y.detach().cpu().clone().numpy()[:, 0]) # [batch_size, num_targets]
             y_hat.append(y_hat_sample)
+            if model_name == "LSTMCMAL" or model_name == "LSTMGMM":
+                loc.append(loc_batch.detach().cpu().clone().numpy())
+                scale.append(scale_batch.detach().cpu().clone().numpy())
+                w.append(w_batch.detach().cpu().clone().numpy())
+                del loc_batch, scale_batch, w_batch
+            if model_name == "LSTMCMAL":
+                kappa.append(kappa_batch.detach().cpu().clone().numpy())
+                del kappa_batch
+
             del x_d, x_s, y, date, x, sample, pred, y_hat_sample
         
         
@@ -136,21 +154,61 @@ def generate_netcdf(path_to_model_dict: Path) -> Tuple[str, xr.Dataset]:
         }
         del dates, y_obs, y_hat
 
+        if model_name == "LSTMCMAL" or model_name == "LSTMGMM":
+            out[basin]["loc"] = np.concatenate(loc)
+            out[basin]["scale"] = np.concatenate(scale)
+            out[basin]["w"] = np.concatenate(w)
+            del loc, scale, w
+        if model_name == "LSTMCMAL":
+            out[basin]["kappa"] = np.concatenate(kappa)
+            del kappa
+
     # Reshape results
     dates = out[basins[0]]["dates"].flatten()
     y_obs = np.stack(([out[basin]["y_obs"] for basin in out.keys()]), axis=0)
     y_hat = np.stack(([out[basin]["y_hat"] for basin in out.keys()]), axis=0)
 
-    ds = xr.Dataset(
-        data_vars={
-            "y_obs": (("basin", "date"), y_obs),
-            "y_hat": (("basin", "date", "samples"), y_hat),
-        },
-        coords={"date": dates, "basin": list(out.keys())},
-    )
+    if model_name == "LSTMCMAL" or model_name == "LSTMGMM":
+        loc = np.stack(([out[basin]["loc"] for basin in out.keys()]), axis=0)
+        scale = np.stack(([out[basin]["scale"] for basin in out.keys()]), axis=0)
+        w = np.stack(([out[basin]["w"] for basin in out.keys()]), axis=0)
+    if model_name == "LSTMCMAL":
+        kappa = np.stack(([out[basin]["kappa"] for basin in out.keys()]), axis=0)
+
+    # LSTM and vLSTM
+    if model_name in ["LSTM", "vLSTM"]:
+        ds = xr.Dataset(
+            data_vars={
+                "y_obs": (("basin", "date"), y_obs),
+                "y_hat": (("basin", "date", "samples"), y_hat),
+            },
+            coords={"date": dates, "basin": list(out.keys())},
+        )
+    elif model_name == "LSTMCMAL":
+        ds = xr.Dataset(
+            data_vars={
+                "y_obs": (("basin", "date"), y_obs),
+                "y_hat": (("basin", "date", "samples"), y_hat),
+                "loc": (("basin", "date", "loc"), loc),
+                "scale": (("basin", "date", "scale"), scale),
+                "kappa": (("basin", "date", "kappa"), kappa),
+                "w": (("basin", "date", "w"), w),
+            },
+            coords={"date": dates, "basin": list(out.keys())},
+        )
+    elif model_name == "LSTMGMM":
+        ds = xr.Dataset(
+            data_vars={
+                "y_obs": (("basin", "date"), y_obs),
+                "y_hat": (("basin", "date", "samples"), y_hat),
+                "loc": (("basin", "date", "loc"), loc),
+                "scale": (("basin", "date", "scale"), scale),
+                "w": (("basin", "date", "w"), w),
+            },
+            coords={"date": dates, "basin": list(out.keys())},
+        )
 
     return experiment_name, ds
-
 
 def generate_metrics(ds: xr.Dataset, name: str, path_metrics: Path) -> None:
     experiment_name = name
@@ -169,21 +227,42 @@ def generate_metrics(ds: xr.Dataset, name: str, path_metrics: Path) -> None:
         # Read basin data
         data = ds.sel(basin=basin)
 
+        # Take mean for MDNs
+        if model in ["LSTMCMAL", "LSTMGMM"]:
+            y_hat_mean = data.y_hat[:, 0]
+            loc, scale, w = data["loc"].values, data["scale"].values, data["w"].values
+            if model == "LSTMCMAL":
+                kappa = data["kappa"].values
+                params = (loc, scale, kappa)
+                dist_arg = dist["laplace"]
+            else:
+                params = (loc, scale, None)
+                dist_arg = dist["gaussian"]
+            
+            # Turn into tensors
+            params = tuple(torch.tensor(param, requires_grad=False) if param is not None else None for param in params)
+            w = torch.tensor(w, requires_grad=False)
+        else:
+            y_hat_mean = data.y_hat.mean(dim="samples")
+
         # Metrics
         metrics[experiment_name][basin] = {}
-        metrics[experiment_name][basin]["NSE"] = calc_metrics.nse(data.y_obs, data.y_hat.mean(dim="samples"))
-        metrics[experiment_name][basin]["KGE"] = calc_metrics.kge(data.y_obs, data.y_hat.mean(dim="samples"))
-        metrics[experiment_name][basin]["CORR"] = calc_metrics.pearsonr(data.y_obs, data.y_hat.mean(dim="samples"))
-        metrics[experiment_name][basin]["a_NSE"] = calc_metrics.alpha_nse(data.y_obs, data.y_hat.mean(dim="samples"))
-        metrics[experiment_name][basin]["b_NSE"] = calc_metrics.beta_nse(data.y_obs, data.y_hat.mean(dim="samples"))
-        metrics[experiment_name][basin]["FHV"] = float(calc_metrics.fdc_fhv(data.y_obs, data.y_hat.mean(dim="samples")))
-        metrics[experiment_name][basin]["FLV"] = float(calc_metrics.fdc_flv(data.y_obs, data.y_hat.mean(dim="samples")))
-        metrics[experiment_name][basin]["FMS"] = float(calc_metrics.fdc_fms(data.y_obs, data.y_hat.mean(dim="samples")))
+        metrics[experiment_name][basin]["NSE"] = calc_metrics.nse(data.y_obs, y_hat_mean)
+        metrics[experiment_name][basin]["KGE"] = calc_metrics.kge(data.y_obs, y_hat_mean)
+        metrics[experiment_name][basin]["CORR"] = calc_metrics.pearsonr(data.y_obs, y_hat_mean)
+        metrics[experiment_name][basin]["a_NSE"] = calc_metrics.alpha_nse(data.y_obs, y_hat_mean)
+        metrics[experiment_name][basin]["b_NSE"] = calc_metrics.beta_nse(data.y_obs, y_hat_mean)
+        metrics[experiment_name][basin]["FHV"] = float(calc_metrics.fdc_fhv(data.y_obs, y_hat_mean))
+        metrics[experiment_name][basin]["FLV"] = float(calc_metrics.fdc_flv(data.y_obs, y_hat_mean))
+        metrics[experiment_name][basin]["FMS"] = float(calc_metrics.fdc_fms(data.y_obs, y_hat_mean))
         if model not in ["LSTM"]:
-            metrics[experiment_name][basin]["LOGLIK"] = calc_kde_loglik(data.y_obs.values, data.y_hat.values)
+            metrics[experiment_name][basin]["LOGLIK_KDE"] = calc_kde_loglik(data.y_obs.values, data.y_hat.values)
             metrics[experiment_name][basin]["CRPS"] = calc_crps(data.y_obs.values, data.y_hat.values)
+        if model in ["LSTMCMAL", "LSTMGMM"]:
+            loglik = loss_nll((params, w), torch.tensor(data.y_obs.values, requires_grad=False).reshape(-1, 1), dist_arg)
+            metrics[experiment_name][basin]["LOGLIK_TRU"] = float(loglik.item())
         else:
-            metrics[experiment_name][basin]["LOGLIK"] = float("nan")
+            metrics[experiment_name][basin]["LOGLIK_KDE"] = float("nan")
             metrics[experiment_name][basin]["CRPS"] = float("nan")
         
     with Path.open(path_metrics, "w") as f:
@@ -210,7 +289,9 @@ def main():
 
             if save_netcdf:
                 path_experiment = path_experiment.parent.parent / f"res_{name}.nc"
-                results.to_netcdf(path_experiment)
+                compression = {var: {"zlib":True, "complevel":5, "dtype":"f4"} for var in results.data_vars}
+                results.to_netcdf(path_experiment, encoding=compression)
+                
         case ".nc":
             name = path_experiment.name.split(".")[0][4:]
             results = xr.open_dataset(path_experiment)
