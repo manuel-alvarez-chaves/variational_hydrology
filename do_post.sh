e0d0a9f7c3274df43586399a688601a103f4325a
manuel-alvarez-chaves <malvarez062@gmail.com>
2025-05-18 13:40:03 +0200

    Postprocessing bash script

---
+++ do_post.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# # # LSTM
+# uv run scripts/postprocess.py experiments/LSTM-256_NSE_S01_531_2025-03-31_14-18-01/model_epoch_32.pt;
+# uv run scripts/postprocess.py experiments/LSTM-256_NSE_S02_531_2025-03-31_18-48-53/model_epoch_36.pt;
+# uv run scripts/postprocess.py experiments/LSTM-256_NSE_S03_531_2025-04-01_17-45-07/model_epoch_34.pt;
+# uv run scripts/postprocess.py -s experiments/LSTM-256_NSE_S04_531_2025-04-01_22-12-15/model_epoch_38.pt;
+# uv run scripts/postprocess.py experiments/LSTM-256_NSE_S05_531_2025-04-01_22-12-15/model_epoch_40.pt;
+# uv run scripts/postprocess.py experiments/LSTM-256_NSE_S06_531_2025-04-01_22-12-15/model_epoch_32.pt;
+# uv run scripts/postprocess.py experiments/LSTM-256_NSE_S07_531_2025-04-01_22-12-15/model_epoch_36.pt;
+# uv run scripts/postprocess.py experiments/LSTM-256_NSE_S08_531_2025-04-01_22-12-15/model_epoch_38.pt;
+# uv run scripts/postprocess.py experiments/LSTM-256_NSE_S09_531_2025-04-01_22-12-15/model_epoch_32.pt;
+# uv run scripts/postprocess.py experiments/LSTM-256_NSE_S10_531_2025-04-03_19-01-35/model_epoch_36.pt;
+# uv run scripts/postprocess.py experiments/LSTM-256_NSE_S11_531_2025-04-03_19-30-59/model_epoch_10.pt;
+# # # LSTMCMAL
+uv run scripts/postprocess.py experiments/LSTMCMAL-250-03_NLL-LAPLACE_S01_531_2025-05-07_19-19-54/model_epoch_04.pt;
+uv run scripts/postprocess.py experiments/LSTMCMAL-250-03_NLL-LAPLACE_S02_531_2025-05-07_23-23-15/model_epoch_01.pt;
+uv run scripts/postprocess.py experiments/LSTMCMAL-250-03_NLL-LAPLACE_S03_531_2025-05-08_03-29-31/model_epoch_02.pt;
+uv run scripts/postprocess.py experiments/LSTMCMAL-250-03_NLL-LAPLACE_S04_531_2025-05-08_07-33-29/model_epoch_03.pt;
+uv run scripts/postprocess.py experiments/LSTMCMAL-250-03_NLL-LAPLACE_S05_531_2025-05-08_11-39-52/model_epoch_04.pt;
+uv run scripts/postprocess.py experiments/LSTMCMAL-250-03_NLL-LAPLACE_S06_531_2025-05-08_15-48-57/model_epoch_04.pt;
+uv run scripts/postprocess.py experiments/LSTMCMAL-250-03_NLL-LAPLACE_S07_531_2025-05-08_19-55-37/model_epoch_02.pt;
+uv run scripts/postprocess.py experiments/LSTMCMAL-250-03_NLL-LAPLACE_S08_531_2025-05-09_00-05-21/model_epoch_03.pt;
+uv run scripts/postprocess.py experiments/LSTMCMAL-250-03_NLL-LAPLACE_S09_531_2025-05-09_04-09-29/model_epoch_03.pt;
+uv run scripts/postprocess.py -s experiments/LSTMCMAL-250-03_NLL-LAPLACE_S10_531_2025-05-14_13-48-14/model_epoch_02.pt;
+uv run scripts/postprocess.py experiments/LSTMCMAL-250-03_NLL-LAPLACE_S11_531_2025-05-14_15-57-39/model_epoch_07.pt;
+# # # LSTMGMM
+uv run scripts/postprocess.py experiments/LSTMGMM-250-10_NLL-GAUSSIAN_S01_531_2025-05-07_17-20-21/model_epoch_02.pt;
+uv run scripts/postprocess.py experiments/LSTMGMM-250-10_NLL-GAUSSIAN_S02_531_2025-05-07_21-24-09/model_epoch_01.pt;
+uv run scripts/postprocess.py -s experiments/LSTMGMM-250-10_NLL-GAUSSIAN_S03_531_2025-05-08_01-28-39/model_epoch_02.pt;
+uv run scripts/postprocess.py experiments/LSTMGMM-250-10_NLL-GAUSSIAN_S04_531_2025-05-08_05-33-40/model_epoch_02.pt;
+uv run scripts/postprocess.py experiments/LSTMGMM-250-10_NLL-GAUSSIAN_S05_531_2025-05-08_09-39-38/model_epoch_04.pt;
+uv run scripts/postprocess.py experiments/LSTMGMM-250-10_NLL-GAUSSIAN_S06_531_2025-05-08_13-45-22/model_epoch_02.pt;
+uv run scripts/postprocess.py experiments/LSTMGMM-250-10_NLL-GAUSSIAN_S07_531_2025-05-08_17-55-36/model_epoch_02.pt;
+uv run scripts/postprocess.py experiments/LSTMGMM-250-10_NLL-GAUSSIAN_S08_531_2025-05-08_22-02-02/model_epoch_03.pt;
+uv run scripts/postprocess.py experiments/LSTMGMM-250-10_NLL-GAUSSIAN_S09_531_2025-05-09_02-11-33/model_epoch_02.pt;
+uv run scripts/postprocess.py experiments/LSTMGMM-250-10_NLL-GAUSSIAN_S10_531_2025-05-09_06-12-50/model_epoch_03.pt;
+uv run scripts/postprocess.py experiments/LSTMGMM-250-10_NLL-GAUSSIAN_S11_531_2025-05-09_10-20-30/model_epoch_02.pt;
+# # # VLSTM-PRO
+uv run scripts/postprocess.py experiments/VLSTM-250-PRO_NLL-GAUSS-KLD_S01_531_2025-04-03_20-09-22/model_epoch_01.pt;
+uv run scripts/postprocess.py experiments/VLSTM-250-PRO_NLL-GAUSS-KLD_S02_531_2025-04-03_21-17-49/model_epoch_03.pt;
+uv run scripts/postprocess.py experiments/VLSTM-250-PRO_NLL-GAUSS-KLD_S03_531_2025-04-06_07-45-15/model_epoch_01.pt;
+uv run scripts/postprocess.py experiments/VLSTM-250-PRO_NLL-GAUSS-KLD_S04_531_2025-04-06_07-45-15/model_epoch_02.pt;
+uv run scripts/postprocess.py experiments/VLSTM-250-PRO_NLL-GAUSS-KLD_S05_531_2025-04-06_07-47-10/model_epoch_02.pt;
+uv run scripts/postprocess.py -s experiments/VLSTM-250-PRO_NLL-GAUSS-KLD_S06_531_2025-04-06_07-47-10/model_epoch_01.pt;
+uv run scripts/postprocess.py experiments/VLSTM-250-PRO_NLL-GAUSS-KLD_S07_531_2025-04-06_07-50-55/model_epoch_01.pt;
+uv run scripts/postprocess.py experiments/VLSTM-250-PRO_NLL-GAUSS-KLD_S08_531_2025-04-06_07-50-55/model_epoch_02.pt;
+uv run scripts/postprocess.py experiments/VLSTM-250-PRO_NLL-GAUSS-KLD_S09_531_2025-04-06_07-56-43/model_epoch_01.pt;
+uv run scripts/postprocess.py experiments/VLSTM-250-PRO_NLL-GAUSS-KLD_S10_531_2025-04-06_08-26-33/model_epoch_01.pt;
+uv run scripts/postprocess.py experiments/VLSTM-250-PRO_NLL-GAUSS-KLD_S11_531_2025-04-06_08-39-06/model_epoch_01.pt;
+# # # VLSTM-DENSE
+uv run scripts/postprocess.py experiments/VLSTM-250-DENSE_NLL-KDE-KLD_S01_531_2025-04-16_15-00-22/model_epoch_05.pt;
+uv run scripts/postprocess.py experiments/VLSTM-250-DENSE_NLL-KDE-KLD_S02_531_2025-04-16_17-37-44/model_epoch_03.pt;
+uv run scripts/postprocess.py experiments/VLSTM-250-DENSE_NLL-KDE-KLD_S03_531_2025-04-16_17-42-24/model_epoch_02.pt;
+uv run scripts/postprocess.py experiments/VLSTM-250-DENSE_NLL-KDE-KLD_S04_531_2025-04-16_17-46-19/model_epoch_04.pt;
+uv run scripts/postprocess.py -s experiments/VLSTM-250-DENSE_NLL-KDE-KLD_S05_531_2025-04-16_17-49-51/model_epoch_03.pt;
+uv run scripts/postprocess.py experiments/VLSTM-250-DENSE_NLL-KDE-KLD_S06_531_2025-04-18_13-57-12/model_epoch_03.pt;
+uv run scripts/postprocess.py experiments/VLSTM-250-DENSE_NLL-KDE-KLD_S07_531_2025-04-18_14-00-23/model_epoch_05.pt;
+uv run scripts/postprocess.py experiments/VLSTM-250-DENSE_NLL-KDE-KLD_S08_531_2025-04-18_14-03-26/model_epoch_03.pt;
+uv run scripts/postprocess.py experiments/VLSTM-250-DENSE_NLL-KDE-KLD_S09_531_2025-04-18_14-06-24/model_epoch_04.pt;
+uv run scripts/postprocess.py experiments/VLSTM-250-DENSE_NLL-KDE-KLD_S10_531_2025-04-18_14-09-32/model_epoch_02.pt;
+uv run scripts/postprocess.py experiments/VLSTM-250-DENSE_NLL-KDE-KLD_S11_531_2025-04-13_11-50-10/model_epoch_03.pt;
