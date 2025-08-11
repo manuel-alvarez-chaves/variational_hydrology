492df59605873cba0176f3485873d607d8b69274
manuel-alvarez-chaves <malvarez062@gmail.com>
2025-08-11 16:04:57 +0200

    PICP and PIT

--- scripts/calc-reliability.py
+++ scripts/calc-reliability.py
@@ -7,9 +7,9 @@ import xarray as xr
 from tqdm import tqdm
 
 
-def calc_reliability_benchmark(data):
+def calc_reliability(data):
     basin_ids = [str(basin) for basin in data.coords["basin"].values]
-    alphas = np.array([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
+    alphas = np.array([0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
     
     alpha_values = 1 - alphas
     lower_quantiles = alpha_values / 2
@@ -24,19 +24,36 @@ def calc_reliability_benchmark(data):
         # Extract data (to numpy arrays)
         y_obs = basin_data["y_obs"].values
         y_hat = basin_data["y_hat"].values
+        _, n_samples = y_hat.shape
         
+        ### PICP
         # Calculate all quantiles at once
         model_lowers = np.quantile(y_hat, lower_quantiles, axis=1)
         model_uppers = np.quantile(y_hat, upper_quantiles, axis=1)
         
         # For each alpha, determine how many observations fall within the interval
-        basin_cdf = np.empty(len(alphas))
+        picp_coverage = np.empty(len(alphas))
         for idx, _ in enumerate(alphas):
             model_is_between = (model_lowers[idx] <= y_obs) & (y_obs <= model_uppers[idx])
-            basin_cdf[idx] = np.mean(model_is_between)
+            picp_coverage[idx] = np.mean(model_is_between)
 
-        results["basins"][basin] = basin_cdf.tolist()
+        results["basins"][basin] = {}
+        results["basins"][basin]["PICP"] = picp_coverage.tolist()
 
+        ### PIT
+        pit_values = np.zeros_like(y_obs)
+    
+        for idx, _ in enumerate(y_obs):
+            obs = y_obs[idx]
+            forecast_dist = y_hat[idx, :]
+            
+            rank = np.sum(forecast_dist <= obs)
+            u = np.random.uniform(0, 1) # prevents ties
+            pit_values[idx] = (rank + u) / (n_samples + 1)
+        
+        pit_coverage = np.array([np.mean(pit_values <= q) for q in alphas])
+        results["basins"][basin]["PIT"] = pit_coverage.tolist()
+        
     return results
 
 
@@ -51,7 +68,7 @@ def main():
     name = path_netcdf.name.split("_")[1]
 
     data = xr.open_dataset(path_netcdf)
-    res = calc_reliability_benchmark(data)
+    res = calc_reliability(data)
     
     # Dump to json
     with open(path_netcdf.parent / f"reliability-{name}.json", "w") as f:
